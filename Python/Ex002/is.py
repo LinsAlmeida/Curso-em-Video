476a829d3d8b803e44a162e1cdafc3ac16d2d4be
Lucas Lins de Albuquerque Maranhão Almeida Vieira <99929629+LinsAlmeida@users.noreply.github.com>
2022-03-08 20:44:22 -0300

    atividades

---
+++ Python/Ex002/is.py
@@ -0,0 +1,5 @@
+#valor = input('digite algo por favor: ')
+#print (type(valor))
+
+valor2 = int(input('digite o valor desejado por favor: '))
+print (type(valor2))
---
+++ Python/Ex002/soma.py
@@ -0,0 +1,4 @@
+n1 = int(input('digite o primeiro n°: '))
+n2 = int(input('digite o segundo n°: '))
+soma = (n1+n2)
+print ('a soma entre {} e {} é {}'.format(n1, n2, soma))
