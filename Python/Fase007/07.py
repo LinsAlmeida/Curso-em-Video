3c32d3e2f031cb0733bfdde42601efa79a7a252a
Lucas Lins de Albuquerque Maranhão Almeida Vieira <99929629+LinsAlmeida@users.noreply.github.com>
2022-03-16 20:42:20 -0300

    operadores numericos

---
+++ Python/Fase007/07.py
@@ -0,0 +1,9 @@
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite outro valor: '))
+soma = n1 + n2
+multi = n1 * n2
+div = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('a soma {}, o produto {} e a divisão {:.3f}'.format(soma, multi, div))
+print ('Divisão inteira {} e a potencia {}'.format(di, e))
