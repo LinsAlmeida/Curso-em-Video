934ad3f6477ebb44f13b3f902aaa4216796a92be
Lucas Lins de Albuquerque Maranhão Almeida Vieira <99929629+LinsAlmeida@users.noreply.github.com>
2022-03-17 20:24:07 -0300

    calculo de desconto de produto

---
+++ Python/Fase007/desafio/def012.py
@@ -0,0 +1,7 @@
+p = input('digite o nome do produto: ')
+v = float(input('digite o valor do produto: '))
+d = 5
+
+soma = d/100*v-v
+
+print(' o Produto {} custa {} mas fica {} com desconto aplicado'.format(p,v,soma))
