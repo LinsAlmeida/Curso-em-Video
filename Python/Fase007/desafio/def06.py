40ecc0f2735c8281e2fe05462a5f3641d09b0039
Lucas Lins de Albuquerque Maranhão Almeida Vieira <99929629+LinsAlmeida@users.noreply.github.com>
2022-03-28 20:12:29 -0300

    Update def06.py

---
+++ Python/Fase007/desafio/def06.py
@@ -0,0 +1,6 @@
+n = int(input('digite um numero'))
+d = n *2
+t = n * 3
+r = n ** (1/2)
+print (' O DOBRO {} VALE {}'.FORMAT(n, d))
+print ('o triplo de{} vale {}. \n A raiz quadrada de {} vale {:.2f}'.format (n, t, n, r))
