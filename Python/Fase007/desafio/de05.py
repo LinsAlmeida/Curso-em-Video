07905f94e9a937249400c26625d4ae9e6a688ab8
Lucas Lins de Albuquerque Maranhão Almeida Vieira <99929629+LinsAlmeida@users.noreply.github.com>
2022-03-28 19:55:59 -0300

    Update de05.py

--- Python/Fase007/desafio/de05.py
+++ Python/Fase007/desafio/de05.py
@@ -1,4 +1,7 @@
-n = int(input('digite umm numero: '))
-soma = n + 1
-sub = n - 1
-print(' esse é o {} o antecessor dele é {} e o sucessor {}'. format(n,sub,soma))
+#n = int(input('digite umm numero: '))
+#soma = n + 1
+#sub = n - 1
+#print(' esse é o {} o antecessor dele é {} e o sucessor {}'. format(n,sub,soma))
+
+n = int(input('digite um numero'))
+print(' esse é o {} o antecessor dele é {} e o sucessor {}'. format(n,(n-1),(n+1)))
